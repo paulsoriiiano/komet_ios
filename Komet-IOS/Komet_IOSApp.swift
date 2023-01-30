5a0f63a25baf3645d15728c82285f74933161dcb
paulsoriiiano <53189056+paulsoriiiano@users.noreply.github.com>
2023-01-29 23:42:34 -0800

    Initial Commit

---
+++ Komet-IOS/Komet_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Komet_IOSApp.swift
+//  Komet-IOS
+//
+//  Created by Paul Junver Soriano on 1/29/23.
+//
+
+import SwiftUI
+
+@main
+struct Komet_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
